0a5ae558848a13c333da5b25ff66a9b8ff9f28f9
Alexandr Ilushechkin <progruzovik@gmail.com>
2017-08-26 17:09:38 +0300

    Improved Rectangle

--- src/game/Rectangle.ts
+++ src/game/Rectangle.ts
@@ -2,12 +2,43 @@ import * as PIXI from "pixi.js"
 
 export class Rectangle extends PIXI.Graphics {
 
-    constructor(width: number, height: number, color: number = 0x000000) {
+    constructor(private _width: number, private _height: number, private _color: number = 0x000000) {
         super();
-        this.drawRectangle(color, width, height);
+        this.redraw(this.color, this.width, this.height);
     }
 
-    drawRectangle(color: number, width: number = this.width, height: number = this.height) {
+    get color(): number {
+        return this._color;
+    }
+
+    set color(value: number) {
+        this._color = value;
+        this.redraw(value, this.width, this.height);
+    }
+
+    get width(): number {
+        return this._width;
+    }
+
+    set width(value: number) {
+        this._width = value;
+        this.redraw(this.color, value, this.height);
+    }
+
+    get height(): number {
+        return this._height;
+    }
+
+    set height(value: number) {
+        this._height = value;
+        this.redraw(this.color, this.width, value);
+    }
+
+    set size(value: PIXI.Point) {
+        this.redraw(this.color, value.x, value.y);
+    }
+
+    private redraw(color: number, width: number, height: number) {
         this.clear();
         this.beginFill(color);
         this.drawRect(0, 0, width, height);
