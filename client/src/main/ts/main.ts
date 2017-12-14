5191196bfe294a046b63703636466e594407826f
Alexandr Ilushechkin <progruzovik@gmail.com>
2017-12-14 22:04:05 +0300

    Moved to main module work with scale modes

--- client/src/main/ts/main.ts
+++ client/src/main/ts/main.ts
@@ -1,5 +1,7 @@
 import BattleApp from "./Battle/BattleApp";
+import * as PIXI from "pixi.js";
 
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 PIXI.utils.skipHello();
 window.onresize = () => battle.resize();
 
