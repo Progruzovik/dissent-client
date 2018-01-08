bb4d7905fea877fc435985bec3da7a44bc6fe31a
Alexandr Ilushechkin <progruzovik@gmail.com>
2018-01-08 18:55:50 +0300

    Fixed MessageHandlerTest

--- server/src/test/java/net/progruzovik/dissent/socket/MessageHandlerTest.java
+++ server/src/test/java/net/progruzovik/dissent/socket/MessageHandlerTest.java
@@ -1,13 +1,15 @@
 package net.progruzovik.dissent.socket;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import net.progruzovik.dissent.dao.TextureDao;
 import net.progruzovik.dissent.captain.Player;
 import net.progruzovik.dissent.captain.SessionPlayer;
+import net.progruzovik.dissent.dao.TextureDao;
 import net.progruzovik.dissent.model.Message;
+import net.progruzovik.dissent.socket.model.MessageSender;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.util.HashMap;
@@ -17,27 +19,27 @@ import static org.mockito.Mockito.*;
 
 public final class MessageHandlerTest {
 
-    private WebSocketSession session;
-    private final Map<String, Object> sessionAttributes = new HashMap<>(1);
-
-    private final Player player = mock(Player.class);
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final MessageHandler messageHandler = new MessageHandler(objectMapper, mock(TextureDao.class));
+    private final WebSocketSession session = mock(WebSocketSession.class);;
 
     public MessageHandlerTest() {
+        final Map<String, Object> sessionAttributes = new HashMap<>(1);
+        final Player player = mock(Player.class);
+        when(player.getMessageSender()).thenReturn(new MessageSender(objectMapper));
         sessionAttributes.put(SessionPlayer.NAME, player);
+        when(session.getAttributes()).thenReturn(sessionAttributes);
     }
 
     @Before
     public void setUp() {
-        session = mock(WebSocketSession.class);
-        when(session.getAttributes()).thenReturn(sessionAttributes);
+        messageHandler.afterConnectionEstablished(session);
     }
 
     @Test
     public void handleStatusMessage() throws Exception {
         final Message message = new Message<>("requestStatus", null);
         messageHandler.handleTextMessage(session, new TextMessage(objectMapper.writeValueAsString(message)));
-        verify(player).getMessageSender();
+        verify(session).sendMessage(any(WebSocketMessage.class));
     }
 }
