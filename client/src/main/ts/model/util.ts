c1286cb97168fb40cebe98d57ae7057c46706bd6
Alexandr Ilushechkin <progruzovik@gmail.com>
2018-04-02 11:14:40 +0300

    Small improvements

--- client/src/main/ts/model/util.ts
+++ client/src/main/ts/model/util.ts
@@ -22,22 +22,22 @@ export const enum Side {
 }
 
 export interface Gun {
-    readonly id: number,
-    readonly name: string,
-    readonly shotCost: number,
-    readonly damage: number,
-    readonly radius: number,
-    readonly typeName: GunType,
+    readonly id: number;
+    readonly name: string;
+    readonly shotCost: number;
+    readonly damage: number;
+    readonly radius: number;
+    readonly typeName: GunType;
     readonly texture: Texture;
 }
 
 export interface Hull {
-    readonly id: number,
-    readonly name: string,
-    readonly actionPoints: number,
-    readonly strength: number,
-    readonly width: number,
-    readonly height: number,
+    readonly id: number;
+    readonly name: string;
+    readonly actionPoints: number;
+    readonly strength: number;
+    readonly width: number;
+    readonly height: number;
     readonly texture: Texture;
 }
 
